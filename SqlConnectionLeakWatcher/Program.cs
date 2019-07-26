1cea15259d6e89dc613a6531751c58009bb718fb
Divya Prakash Jha <dpjha84@gmail.com>
2019-07-26 14:49:34 +0530

    Add project files.

---
+++ SqlConnectionLeakWatcher/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlConnectionLeakWatcher
+{
+    internal class Watcher
+    {
+        static int counter = 0;
+        readonly SqlConnection conn;
+
+        internal Watcher(SqlConnection conn)
+        {
+            this.conn = conn;
+        }
+
+        internal void StartWatch()
+        {
+            conn.StateChange += Conn_StateChange;
+            counter++;
+        }
+
+        internal void StopWatch()
+        {
+            conn.StateChange -= Conn_StateChange;
+        }
+
+
+        private void Conn_StateChange(object sender, System.Data.StateChangeEventArgs e)
+        {
+            Console.WriteLine($"{e.CurrentState} {counter}");
+        }
+    }
+
+    public class WatchHandler
+    {
+        private readonly Watcher watcher;
+
+        public WatchHandler(SqlConnection conn)
+        {
+            watcher = new Watcher(conn);
+        }
+
+        public void StartWatch()
+        {
+            watcher.StartWatch();
+        }
+        public void StopWatch()
+        {
+            watcher.StopWatch();
+        }
+    }
+}
---
+++ SqlConnectionLeakWatcherClient/Program.cs
@@ -0,0 +1,39 @@
+﻿using SqlConnectionLeakWatcher;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlConnectionLeakWatcherClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            int max = 10;
+            var randomList = new List<int> { random.Next(1, max), random.Next(1, max), random.Next(1, max) };
+            for (int i = 1; i <= max; i++)
+            {
+                SqlConnection conn = null;
+                WatchHandler handler = null;
+                try
+                {
+                    conn = new SqlConnection("Data Source=DESKTOP-O80CRIA;Initial Catalog=Emp;Integrated Security=True");
+                    handler = new WatchHandler(conn);
+                    handler.StartWatch();
+                    conn.Open();
+                }
+                finally
+                {
+                    //handler.StopWatch();
+                    if (!randomList.Contains(i))
+                        conn.Dispose();
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
